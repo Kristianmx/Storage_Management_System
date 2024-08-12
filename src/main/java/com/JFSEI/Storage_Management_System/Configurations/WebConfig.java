c66f096a57b2e92a47d6f97ad9946a9127046f41
Kristiamnx <rivaskristopher24@gmail.com>
2024-08-12 15:39:21 -0500

    CORS configure

---
+++ src/main/java/com/JFSEI/Storage_Management_System/Configurations/WebConfig.java
@@ -0,0 +1,24 @@
+package com.JFSEI.Storage_Management_System.Configurations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Bean
+    public WebMvcConfigurer corsConfigure() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:5500") // Cambia esto por el origen que necesites
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
